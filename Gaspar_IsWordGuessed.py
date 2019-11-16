a32e99c1089da57926a904001f2dc761e0ad031f
Summersea-prog <57347341+Summersea-prog@users.noreply.github.com>
2019-11-16 23:48:16 +0800

    Add files via upload

---
+++ Gaspar_IsWordGuessed.py
@@ -0,0 +1,6 @@
+def isWordGuessed (secretWord, letterGuessed):
+	for x in secretWord:
+		if x is letterGuessed:
+			bool(letterGuesed)
+		else:
+			return("false")
---
+++ Gaspar_getAvailableLetters.py
@@ -0,0 +1,6 @@
+import string
+def getAvailableLetters(lettersGuessed):
+	s=list(string.ascii_lowercase)
+	for x in lettersGuessed:
+		s.remove(x)
+	print(s)
